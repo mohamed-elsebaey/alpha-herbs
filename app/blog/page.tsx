7a4602c5fdb51ae163ad8f492ba1fb9cc0c0165f
Mohamed <elsebaey95@outlook.com>
2024-08-31 17:51:54 +0300

    add dynamic page again

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,8 +1,15 @@
-import Blogs from "@/components/blogs/Blogs";
 import React from "react";
+// import type { Metadata } from "next";
 
-function page() {
+import Blogs from "@/components/blogs/Blogs";
+
+export const metadata = {
+  title: "Herb of the Day: Discover its Amazing Health Benefits",
+  description: "Explore the world of medicinal and aromatic plants...",
+};
+
+function BlogPage() {
   return <Blogs />;
 }
 
-export default page;
+export default BlogPage;
