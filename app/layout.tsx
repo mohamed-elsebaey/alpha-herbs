9e1cc87b3bf98251db8eca1692e594251de34ebe
Mohamed <elsebaey95@outlook.com>
2024-09-09 17:12:33 +0300

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`${open_Sans.className} mx-auto max-w-[1500px]`}>
+      <body className={`${open_Sans.className} mx-auto max-w-[1800px]`}>
         <Header />
         {/* Teplate used to add framer motion to evry child */}
         <Template>{children}</Template>
