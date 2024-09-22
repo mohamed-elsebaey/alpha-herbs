8021b7e95234534ab56b72a99fc622abb9968d0d
Mohamed <elsebaey95@outlook.com>
2024-09-22 16:28:57 +0300

    add description to main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,13 @@ import Products from "@/components/products/Product";
 import Video from "@/components/Video";
 import Stats from "@/components/stats/Stats";
 import Images from "@/components/Images";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Alpha Herbs",
+  description:
+    "Build your natural products with Alpha Herbs. We are a leading supplier of bulk medicinal and aromatic herbs. We provide customized solutions to meet your needs, whether you are a functional food producer, herbal medicine manufacturer, or cosmetics company. Our product quality is our guarantee of your success.",
+};
 
 export default function Home() {
   return (
