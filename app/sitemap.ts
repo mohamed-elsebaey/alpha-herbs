58897a0a283f17e77ae77544c4044b2cb263a4f1
Mohamed <elsebaey95@outlook.com>
2024-09-26 14:16:48 +0300

    update lastModified

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,18 +2,18 @@ export default async function sitemap() {
   const baseUrl = "https://www.alpha-herbs.com";
 
   return [
-    { url: baseUrl, lastMofified: new Date() },
-    { url: `${baseUrl}/about`, lastMofified: new Date() },
-    { url: `${baseUrl}/blog`, lastMofified: new Date() },
-    { url: `${baseUrl}/contact`, lastMofified: new Date() },
-    { url: `${baseUrl}/faq`, lastMofified: new Date() },
-    { url: `${baseUrl}/profile`, lastMofified: new Date() },
-    { url: `${baseUrl}/sign-in`, lastMofified: new Date() },
-    { url: `${baseUrl}/sign-up`, lastMofified: new Date() },
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/about`, lastModified: new Date() },
+    { url: `${baseUrl}/blog`, lastModified: new Date() },
+    { url: `${baseUrl}/contact`, lastModified: new Date() },
+    { url: `${baseUrl}/faq`, lastModified: new Date() },
+    { url: `${baseUrl}/profile`, lastModified: new Date() },
+    { url: `${baseUrl}/sign-in`, lastModified: new Date() },
+    { url: `${baseUrl}/sign-up`, lastModified: new Date() },
 
     {
       url: `${baseUrl}/blog/mint-natures-refreshing-remedy-for-digestion-and-more`,
-      lastMofified: new Date(),
+      lastModified: new Date(),
     },
   ];
 }
