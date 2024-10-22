b56622373f9d73b97a6576fcff0dfaa52a99c0c0
Mohamed <elsebaey95@outlook.com>
2024-10-22 23:32:20 +0300

    likes V2

--- actions/blogActions/addOrRemoveLikeAction.ts
+++ actions/blogActions/addOrRemoveLikeAction.ts
@@ -11,5 +11,5 @@ export async function addOrRemoveLikeAction(articleId: any) {
 
   const likeState = await getArticleLikeStateByUserIdArticleId(userId[0].id,articleId)
 
-  revalidatePath('/blog')
+  revalidatePath("/blog")
 }
