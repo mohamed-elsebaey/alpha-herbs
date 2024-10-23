2474dc1b7a2531a2ab2ef98cb892929586e0e5bf
Mohamed <elsebaey95@outlook.com>
2024-10-23 15:14:47 +0300

    Likes V6

--- components/blogs/card/HeartButton.tsx
+++ components/blogs/card/HeartButton.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { addOrRemoveLikeAction } from "@/actions/blogActions/addOrRemoveLikeAction";
+import { Accessibility } from "@/components/Accessibility";
 import { Heart } from "lucide-react";
+import { redirect } from "next/navigation";
 import { useState } from "react";
 
 function HeartButton({
@@ -15,10 +17,15 @@ function HeartButton({
   const [liked, setLiked] = useState(userIsLiked);
   const [likesNumber, setLikesNumber] = useState(total_names.length);
 
-  const onChangeHeartState = () => {
-    setLiked(() => !liked);
-    setLikesNumber(liked ? likesNumber - 1 : likesNumber + 1);
-    addOrRemoveLikeAction(articleId);
+  const onChangeHeartState = async () => {
+    const isLogin = await Accessibility();
+    if (isLogin) {
+      // setLiked(() => !liked);
+      // setLikesNumber(liked ? likesNumber - 1 : likesNumber + 1);
+      addOrRemoveLikeAction(articleId);
+    } else {
+
+    }
   };
   return (
     <>
